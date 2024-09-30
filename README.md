fa488e26935ee0718dec15a0b9441a8ac77eede2
Rodrigo Zanetti <116608232+RodrigoZanetti175@users.noreply.github.com>
2024-09-30 20:49:51 +0000

    Ajeitando repositório (50%)

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# labyrinth_bruteforce
+# Explorador de Labirintos (Iterativo)
+### Dupla: Rodrigo Zanetti e Vinícius Crozato
+
+## Explicação da Ideia
+
+A ideia foi concebida durante a aula de estrutura de dados, onde, após algumas outras ideias, chegamos a conclusão de que o problema pensado poderia ser resolvido e seria bem interessante para o desenvolvimento do grupo.
+
+## O Problema
+
+Criar um algoritmo que seja capaz de receber um arquivo .txt contendo uma espécie de mapa de um labirinto, onde "1" representa paredes do labirinto, "2" a entrada e saída e "0" representa a trilha que é possível ser explorada. Com os parâmetros recebidos, o algoritmo deve ser capaz de encontrar um dos caminhos possíveis que levam da entrada até a saída do labirinto.
+
+## Ideia de solução
+
+Existem diversas soluções possíveis para o problema, porém, a solução que encontramos, parte do paradigma de iteração, portanto, se trata de uma solução que utiliza iterações para resolução.
+
+Deste modo, chegamos a uma solução que utiliza de alguns parâmetros e conceitos interessantes que pensamos durante o entendimento do problema.
+
+## Solução
+
+- Primeiramente o programa lerá o labirinto e o transformará em uma Lista das linhas do labirinto, onde cada elemento da linha é representado por uma tupla de dois inteiros, ou seja, coordenadas **(l,c)**, onde "l" representa linha e "c" representa coluna.
+
+- Após a leitura do labirinto, o algoritmo passa por uma fase de reconhecimento, onde ele extrairá dados importantes do labirinto, sendo eles:
+    - **Começo e Fim**: Passando pela função `get_start_end()`, o algoritmo registrará as coordenadas da entrada e saída do labirinto.
+
+    - **Pontos de Intersecção**: Com a execução da função `get_intersections()`, o programa obeterá os pontos de intersecção do labirinto, que são a base da função principal de navegação. Os pontos de intersecção são aqueles pontos que dão acesso a mais de um sentido(horizontal e vertical), dando brecha para mais possibilidades de navegação.
+
+- Com os dados principais obtidos, o algoritmo pode iniciar a função principal do programa, que se trata de `navigate()`, a qual recebe .... parâmetros
--- main.py
+++ main.py
@@ -40,8 +40,8 @@ def get_start_end(m: List[List[int]]) -> Tuple[Tuple[int, int], Tuple[int, int]]
                     check = True
                     
                     
-def get_interssections(m: List[List[int]]) -> List[Tuple[int, int]]:
-    interssections = []
+def get_intersections(m: List[List[int]]) -> List[Tuple[int, int]]:
+    intersections = []
     for i in range(len(m)):
         for j in range(len(m[i])):
             vertical = False
@@ -50,8 +50,8 @@ def get_interssections(m: List[List[int]]) -> List[Tuple[int, int]]:
                 horizontal = not m[i-1][j] or not m[i+1][j]
                 vertical = not m[i][j+1] or not m[i][j-1]
             if vertical and horizontal:
-                interssections.append((i,j))                
-    return interssections
+                intersections.append((i,j))                
+    return intersections
 def print_t_map(m: List[List[int]], t: List[Tuple[int,int]]) -> None:
      for i, row in enumerate(m):
         for j, pos in enumerate(row):
@@ -67,7 +67,7 @@ def navigate(
     m: List[List[int]],
     start: Tuple[int,int],
     end: Tuple[int,int],
-    interssections: List[Tuple[int,int]]
+    intersections: List[Tuple[int,int]]
     ) -> List[Tuple[int,int]]:
     current = start
     track: List[Tuple[int,int]]
@@ -75,7 +75,7 @@ def navigate(
     previous: List[Tuple[int,int]]
     previous = []
     i = 0
-    while i <= len(interssections):
+    while i <= len(intersections):
         track.append(current)
         check = False 
         if current[0] == end[0] or current[1] == end[1]:
@@ -93,38 +93,38 @@ def navigate(
                     track.append((end[0]+(j+1),end[1]))   
             #print('c')
             return track
-        if(interssections[i][0] == current[0] and interssections[i] not in previous):
+        if(intersections[i][0] == current[0] and intersections[i] not in previous):
             #print('a ' + str(current))
             check = True
             previous.append(current)
-            if interssections[i][1] > current[1]:
-                for j in range(interssections[i][1] - current[1]):
-                    if(m[interssections[i][0]][current[1]+(j+1)]):
+            if intersections[i][1] > current[1]:
+                for j in range(intersections[i][1] - current[1]):
+                    if(m[intersections[i][0]][current[1]+(j+1)]):
                         check = False 
-                    track.append((interssections[i][0],current[1]+(j+1)))
-            if current[1] > interssections[i][1]:
-                for j in range(current[1] - interssections[i][1] - 1):
-                    if(m[interssections[i][0]][interssections[i][1]+(j+1)]):
+                    track.append((intersections[i][0],current[1]+(j+1)))
+            if current[1] > intersections[i][1]:
+                for j in range(current[1] - intersections[i][1] - 1):
+                    if(m[intersections[i][0]][intersections[i][1]+(j+1)]):
                         check = False
-                    track.append((interssections[i][0], interssections[i][1]+(j+1)))
+                    track.append((intersections[i][0], intersections[i][1]+(j+1)))
                     # Salvar a ultima interssecção e se der errado ler track em ordem reversa para encontrar de onde
                     # é preciso continuar a procurar e excluir todo o caminho feito a partir daquele ponto.
-        if(interssections[i][1] == current[1] and interssections[i] not in previous):
+        if(intersections[i][1] == current[1] and intersections[i] not in previous):
             #print('b ' + str(current))
             previous.append(current)
             check = True
-            if interssections[i][0] > current[0]:
-                for j in range(interssections[i][0]-current[0]-1):
-                    if(m[current[0]+(j+1)][interssections[i][1]]):
+            if intersections[i][0] > current[0]:
+                for j in range(intersections[i][0]-current[0]-1):
+                    if(m[current[0]+(j+1)][intersections[i][1]]):
                         check = False
-                    track.append((current[0]+(j+1),interssections[i][1]))
-            if current[0] > interssections[i][0]:
-                for j in range(current[0]-interssections[i][0]-1):
-                    if(m[interssections[i][0]+(j+1)][interssections[i][1]]):
+                    track.append((current[0]+(j+1),intersections[i][1]))
+            if current[0] > intersections[i][0]:
+                for j in range(current[0]-intersections[i][0]-1):
+                    if(m[intersections[i][0]+(j+1)][intersections[i][1]]):
                         check = False
-                    track.append((interssections[i][0]+(j+1),interssections[i][1]))                    
+                    track.append((intersections[i][0]+(j+1),intersections[i][1]))                    
         elif not check:
-            #É necessário arrumar alguma maneira de ele reitarar pelos valores de interssections
+            #É necessário arrumar alguma maneira de ele reitarar pelos valores de intersections
             #para que ele consiga voltar a valores de "decisão"
 
             #Arrumar maneira de iterar desde  o começo todas as vezes 
@@ -134,16 +134,16 @@ def navigate(
             #print(track)
             track = track[:target_index+1]
             #print(track)
-            interssections.remove(current)
+            intersections.remove(current)
             #print(previous)
-            #print(interssections)
+            #print(intersections)
             previous.pop()
             #print(i)
             current = track[target_index]
             i = 0
             continue
 
-        current = interssections[i]
+        current = intersections[i]
         i += 1
         
         
@@ -159,7 +159,7 @@ def navigate(
    #d -> voltar
 
 if __name__ == "__main__":
-    f = open("./labyrinth2.txt")
+    f = open("./labyrinth3.txt")
     map = []
     for line in f.readlines():
         linha = []
@@ -171,9 +171,9 @@ if __name__ == "__main__":
     
     print_map(map)
     start,end = get_start_end(map)
-    interssections = get_interssections(map)
+    intersections = get_intersections(map)
     print(get_start_end(map))
-    print(get_interssections(map))
-    track = navigate(map, start, end, interssections)
+    print(get_intersections(map))
+    track = navigate(map, start, end, intersections)
     print_t_map(map, track)
     
