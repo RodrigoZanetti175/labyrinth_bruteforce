78790d60165c53061ed51ea91650597eff768fee
Rodrigo <rodrigo.zanetti@hotmail.com>
2024-10-07 14:12:05 -0300

    Testando possibilidades

--- main.py
+++ main.py
@@ -49,6 +49,28 @@ def print_t_map(m: List[List[int]], t: List[Tuple[int,int]]) -> None:
                 print(colors[pos] + str(pos), RESET, end="")
         print("")
 
+def navigate2(
+    m: List[List[int]],
+    start: Tuple[int,int],
+    end: Tuple[int,int],
+    intersections: List[Tuple[int,int]]
+    ) -> List[Tuple[int,int]]:
+    done = False 
+    current = start
+    options: List[Tuple[int,int]]
+    while not done:
+        if current[0] == end[0] or current[1] == end[1]:
+            ######
+            done = True #Fim da navegação
+        for i in intersections:
+            if current[0] == i[0] or current[1] == i[1]:
+                options.append(i)
+        for opt in options:
+            if current[0] == opt[0]:
+                print("a")
+            if current[1] == opt[1]:
+                print("b")
+
 def navigate(
     m: List[List[int]],
     start: Tuple[int,int],
@@ -60,11 +82,20 @@ def navigate(
     track = []
     previous: List[Tuple[int,int]]
     previous = []
+    counter = 0
+    timeout = 0
+    append = True
     i = 0
     while i <= len(intersections):
-        track.append(current)
+        print(current)
+        if append:
+            track.append(current)
         check = False 
+        for inter in intersections:
+            if current[0] == inter[0] or current[1] == inter[1]:
+                counter += 1
         if current[0] == end[0] or current[1] == end[1]:
+            print("c " + str(current))
             if current[0] == end[0] and end[1] > current[1]:
                 for j in range(end[1] - current[1]):
                     track.append((end[0], current[1]+(j+1)))
@@ -79,6 +110,7 @@ def navigate(
                     track.append((end[0]+(j+1),end[1]))   
             return track
         if(intersections[i][0] == current[0] and intersections[i] not in previous):
+            print("a " + str(current))
             check = True
             previous.append(current)
             if intersections[i][1] > current[1]:
@@ -91,7 +123,11 @@ def navigate(
                     if(m[intersections[i][0]][intersections[i][1]+(j+1)]):
                         check = False
                     track.append((intersections[i][0], intersections[i][1]+(j+1)))
+            append = True
+            timeout = 0
+            counter = 0  
         if(intersections[i][1] == current[1] and intersections[i] not in previous):
+            print("b " + str(current))
             previous.append(current)
             check = True
             if intersections[i][0] > current[0]:
@@ -103,21 +139,34 @@ def navigate(
                 for j in range(current[0]-intersections[i][0]-1):
                     if(m[intersections[i][0]+(j+1)][intersections[i][1]]):
                         check = False
-                    track.append((intersections[i][0]+(j+1),intersections[i][1]))                    
+                    track.append((intersections[i][0]+(j+1),intersections[i][1]))
+            append = True  
+            timeout = 0
+            counter = 0                  
         elif not check:
+            if timeout < counter:
+                i+=1
+                timeout += 1
+                append = False
+                continue
+            print("d " + str(current))
+            print(intersections[i])
             target_index = track.index((previous[-1]))
             track = track[:target_index+1]
             intersections.remove(current)
+            print(intersections)
             previous.pop()
             current = track[target_index]
             i = 0
+            timeout = 0
+            counter = 0  
             continue
 
         current = intersections[i]
         i += 1
 
 if __name__ == "__main__":
-    f = open("Labyrinths/labyrinth.txt")
+    f = open("Labyrinths/01.txt")
     map = []
     for line in f.readlines():
         linha = []
